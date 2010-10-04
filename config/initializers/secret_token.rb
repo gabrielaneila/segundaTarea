472b5ccf5a709fec9432f8c70b1f36e5b16e776d
Fernando <fer.petrelli@gmail.com>
2010-10-04 00:24:09 -0300

    TODO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Segundo::Application.config.secret_token = 'a095d35debe652bf8f957054bb5dfd69d1d2cd561027103181fdbc83d2f323074c29a7035fe06faef33d74d952ed01c56c0e49919e51b4ecfc813ca070a208ba'
